59df023c03fe8a20094162a1b253878871850a45
dino abdela delil <104862943+Dinoab@users.noreply.github.com>
2023-07-30 01:20:20 -0700

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,35 +1,15 @@
 #!/usr/bin/python3
-""" UTF-8 validation module. """
+"""
+Main file for testing
+"""
 
-def get_binary(num):
-    """ Return binary representation of a number. """
-    binary = []
-    while(num > 1):
-        binary.append(num % 2)
-        num = num // 2
-    binary.append(num)
-    gap = len(binary) - 8
-    if gap < 0:
-        for i in range(abs(gap)):
-            binary.append(0)
-    binary.reverse()
-    return binary
+validUTF8 = __import__('0-validate_utf8').validUTF8
 
-def validUTF8(data):
-    """ determines if a given data set represents a valid
-        UTF-8 encoding. Return: True if data is a valid
-        UTF-8 encoding, else return False
-        The specification is the follwing:
-            - A character in UTF-8 can be 1 to 4 bytes long
-            - The data set can contain multiple characters
-            - The data will be represented by a list of integers
-            - Each integer represents 1 byte of data, therefore
-              only handle the 8 least significant bits of each
-              integer
-    """
-    utf = [[0], [1, 0], [1, 1, 0], [1, 1, 1, 0]]
-    for d in data:
-        bi = get_binary(d)
-        if not(bi[:1] == utf[0]):
-            return False
-    return True
+data = [65]
+print(validUTF8(data))
+
+data = [80, 121, 116, 104, 111, 110, 32, 105, 115, 32, 99, 111, 111, 108, 33]
+print(validUTF8(data))
+
+data = [229, 65, 127, 256]
+print(validUTF8(data))
